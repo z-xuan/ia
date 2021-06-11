e410ddadfe38ad2c9a9983df0ab8ccfd9cd92f18
z-xuan <zhujian199110@163.com>
2021-06-11 17:46:59 +0800

    11

---
+++ start/src/main/java/com/xuan/test/ia/IaApplication.java
@@ -0,0 +1,13 @@
+package com.xuan.test.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IaApplication.class, args);
+    }
+
+}
---
+++ start/src/test/java/com/xuan/test/ia/IaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuan.test.ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
